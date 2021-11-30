ae5f522302c987ec6c40b0fe314c9d733b0d8d3d
Brndndev <brndndev@outlook.com>
2021-11-29 19:09:42 -0500

    👺 first commit: printed Hello World! ✅: passed

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,11 @@
 function hello_world(){
   // write your code here
-
+  // Call the Function by Name
+  return "Hello World!";
 }
 
 module.exports = hello_world;
+
+
+// consol.log("Hello World!") fails
+//print hello_world() doesn't work
